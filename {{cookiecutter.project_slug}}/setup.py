becbed9224b1897347c9013bc4140bbd365c14e8
Jean-Christophe Malapert <jean-christophe.malapert@cnes.fr>
2021-04-15 18:03:32 +0200

    Get version from git

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -22,8 +22,8 @@ with open(
     exec(f.read(), about)
 
 setuptools.setup(
+    use_scm_version=True,
     name=about["__name_soft__"],
-    version=about["__version__"],
     description=about["__description__"],
     long_description=readme,
     author=about["__author__"],
